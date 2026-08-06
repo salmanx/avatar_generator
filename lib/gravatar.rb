e5429b55b958b6d976bd263edcc522e3b40e7862
Salman Mahmud <salmanmahmud2014@gmail.com>
2026-08-07 00:59:22 +0900

    initialized gravatar gem

---
+++ lib/gravatar.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gravatar/version"
+
+module Gravatar
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/gravatar_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Gravatar do
+  it "has a version number" do
+    expect(Gravatar::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
